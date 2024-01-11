298eba7f996298fcdc73279cd3bceb1bba40ea82
Keanon Born <disposingmoon1@gmail.com>
2024-01-11 12:07:29 -0500

    add

---
+++ C#Variables1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace C_Variables1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lewis";
+            int age = 26;
+            char middleInitial = 'L';
+            bool isOver18 = true;
+            double currentTemp = 63.2;
+            decimal currentPrice = 20.32;
+
+            
+
+            Console.WriteLine($"His name is {name}, he is {age} years old. His middle initial is {middleInitial}")
+    }
+}
